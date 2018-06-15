4f437ca13b926331dd8501a31b9a42b28a57ba45
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2018-06-15 12:02:04 +0200

    D4CRIS-507: - clean code - set term of use as attribute of the request and retrieve it without any check

--- jspui/src/main/java/org/dspace/app/webui/servlet/IIIFImageProxyServlet.java
+++ jspui/src/main/java/org/dspace/app/webui/servlet/IIIFImageProxyServlet.java
@@ -1,14 +1,15 @@
 package org.dspace.app.webui.servlet;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.sql.SQLException;
+import java.util.Random;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
@@ -17,8 +18,11 @@ import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.DSpaceObject;
 import org.dspace.content.Item;
 import org.dspace.core.ConfigurationManager;
+import org.dspace.core.Context;
+import org.dspace.handle.HandleManager;
 
 public class IIIFImageProxyServlet extends ConfigurableProxyServlet {
 	
@@ -45,8 +49,9 @@ public class IIIFImageProxyServlet extends ConfigurableProxyServlet {
 	@Override
 	protected void copyResponseEntity(HttpResponse proxyResponse, HttpServletResponse servletResponse,
 			HttpRequest proxyRequest, HttpServletRequest servletRequest) throws IOException {
-		if ((boolean) servletRequest.getAttribute("require-iiif-auth")) {
-    		servletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		String pattern = (String) servletRequest.getAttribute("require-iiif-auth");
+		if (pattern != null) {
+    		servletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);    		   		
         }
 		else {
 			servletResponse.setStatus(HttpServletResponse.SC_OK);
@@ -85,8 +90,23 @@ public class IIIFImageProxyServlet extends ConfigurableProxyServlet {
 									+ servletRequest.getAttribute("ProxyServletRequestWrapper-requestPath") + relativePath);
 				}
 				
-				Item item = (Item) servletRequest.getAttribute("item");
-				rewriteResponseStr = addIIIFAuthenticationService(proxyResponse, servletResponse, proxyRequest, servletRequest, item, rewriteResponseStr);
+				Context c = null;
+				Item item = null;
+				String handle = (String) servletRequest.getAttribute("handle");
+				try {
+					c = new Context();
+					DSpaceObject dso = HandleManager.resolveToObject(c, handle);
+					if (dso instanceof Item) {
+						item = (Item) dso;
+					}
+				} catch (SQLException e) {
+					log.error(e.getMessage(), e);
+				} finally {
+					if (c != null && c.isValid()) {
+						c.abort();
+					}
+				}
+				rewriteResponseStr = addIIIFAuthenticationService(proxyResponse, servletResponse, proxyRequest, servletRequest, item, pattern, rewriteResponseStr);
 				
                 if (doLog) {
                     log("relativeUri="+relativeUri);
@@ -114,12 +134,12 @@ public class IIIFImageProxyServlet extends ConfigurableProxyServlet {
 	}
 	
 	private String addIIIFAuthenticationService(HttpResponse proxyResponse, HttpServletResponse servletResponse,
-			HttpRequest proxyRequest, HttpServletRequest servletRequest, Item item, String rewriteResponseStr) {
+			HttpRequest proxyRequest, HttpServletRequest servletRequest, Item item, String pattern, String rewriteResponseStr) {
 		if (StringUtils.endsWith(servletRequest.getPathInfo(), "/info.json")) {
-			if (IIIFAuthenticationUtils.clickthroughTermOfUsage(item) != null) {
+			if (StringUtils.contains(pattern, "clickthrough")) {
 				rewriteResponseStr = addClickthroughPattern(proxyResponse, servletResponse, proxyRequest, servletRequest, item, rewriteResponseStr);
 			}
-			else if (StringUtils.equals((String) servletRequest.getAttribute("iiifauth-pattern"), "login")) {
+			else {
 				rewriteResponseStr = addLoginPattern(proxyResponse, servletResponse, proxyRequest, servletRequest, item, rewriteResponseStr);
 			}
 		}
@@ -128,48 +148,17 @@ public class IIIFImageProxyServlet extends ConfigurableProxyServlet {
 	
 	private String addClickthroughPattern(HttpResponse proxyResponse, HttpServletResponse servletResponse,
 			HttpRequest proxyRequest, HttpServletRequest servletRequest, Item item, String rewriteResponseStr) {
-		String clickthrough = ConfigurationManager.getProperty("iiif", "clickthrough");
-		String clickthrough_file = ConfigurationManager.getProperty("iiif", "clickthrough.file");
-		String clickthrough_metadata = ConfigurationManager.getProperty("iiif", "clickthrough.metadata");
-		String clickthrough_metadata_value = null;
-		if (item.getMetadata(clickthrough_metadata) != null) {
-			clickthrough_metadata_value = item.getMetadata(clickthrough_metadata);
-		}
 		rewriteResponseStr = rewriteResponseStr.substring(0, StringUtils.lastIndexOf(rewriteResponseStr, "}"));
-		
-		String clickthrough_default_terms = null;
-		try {
-			FileInputStream inputStream = new FileInputStream(clickthrough_file);
-			clickthrough_default_terms = IOUtils.toString(inputStream);
-			inputStream.close();
-		} catch (IOException e) {
-			log.warn(e.getMessage());
-		}
-		boolean use_default_terms = false;
-		if (StringUtils.equals(clickthrough, "all")) {
-			use_default_terms = true;
-		}
-		else if (StringUtils.equals(clickthrough, "item")) {
-			if (StringUtils.isBlank(clickthrough_metadata_value)) {
-				use_default_terms = true;
-			}
-		}
 		String context = "http://iiif.io/api/auth/1/context.json";
-		String id = "http://scognamiglio.local.it:8080/jspui/json/iiif-cookie?handle=" + item.getHandle();
+		String id = ConfigurationManager.getProperty("dspace.url") + "/json/iiif-cookie?handle=" + item.getHandle();
 		String profile = "http://iiif.io/api/auth/1/clickthrough";
 		String label = "Restricted material";
 		String header = "Terms of usage";
-		String description = null;
-		if (use_default_terms) {
-			description = clickthrough_default_terms.replace("\n", "").replace("\r", "");
-		}
-		else {
-			description = clickthrough_metadata_value;
-		}
+		String description = (String) servletRequest.getAttribute("termOfUse");
 		String confirmLabel = "I Agree";
 		String failureHeader = "Terms of use not accepted!";
 		String failureDescription= "You must accept the terms of use to see the content.";
-		String tokenId = "http://scognamiglio.local.it:8080/jspui/json/token";
+		String tokenId = ConfigurationManager.getProperty("dspace.url") + "/json/iiif-token";
 		String tokenProfile = "http://iiif.io/api/auth/1/token";
 		return writePattern(rewriteResponseStr, context, id, profile, label, header, description,
 				confirmLabel, failureHeader, failureDescription, tokenId, tokenProfile);
@@ -179,7 +168,7 @@ public class IIIFImageProxyServlet extends ConfigurableProxyServlet {
 			HttpRequest proxyRequest, HttpServletRequest servletRequest, Item item, String rewriteResponseStr) {
 		rewriteResponseStr = rewriteResponseStr.substring(0, StringUtils.lastIndexOf(rewriteResponseStr, "}"));
 		String context = "http://iiif.io/api/auth/1/context.json";
-		String id = "http://scognamiglio.local.it:8080/jspui/json/iiif-cookie?login=true&handle=" + item.getHandle(); // change it with .../json/login
+		String id = ConfigurationManager.getProperty("dspace.url") + "/iiif-login?handle=" + item.getHandle();
 		String profile = "http://iiif.io/api/auth/1/login";
 		String label = "Login to DSpace";
 		String header = "Please Log In";
@@ -187,7 +176,7 @@ public class IIIFImageProxyServlet extends ConfigurableProxyServlet {
 		String confirmLabel = "Login";
 		String failureHeader = "Log In failed!";
 		String failureDescription= "You must login to see the content.";
-		String tokenId = "http://scognamiglio.local.it:8080/jspui/json/token";
+		String tokenId = ConfigurationManager.getProperty("dspace.url") + "/json/iiif-token";
 		String tokenProfile = "http://iiif.io/api/auth/1/token";
 		return writePattern(rewriteResponseStr, context, id, profile, label, header, description,
 				confirmLabel, failureHeader, failureDescription, tokenId, tokenProfile);
