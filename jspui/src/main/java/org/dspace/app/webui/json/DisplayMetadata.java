c0b4209113196263f76766fb66ec96ebc9931b5e
Andrea Bollini <andrea.bollini@4science.it>
2018-07-02 21:50:22 +0200

    compatibility with standard dspace

---
+++ jspui/src/main/java/org/dspace/app/webui/json/DisplayMetadata.java
@@ -0,0 +1,8 @@
+package org.dspace.app.webui.json;
+
+public class DisplayMetadata {
+
+	public String value;
+	public String label;
+
+}
---
+++ jspui/src/main/java/org/dspace/app/webui/jsptag/DisplayItemMetadataUtils.java
@@ -0,0 +1,18 @@
+package org.dspace.app.webui.jsptag;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.dspace.app.webui.json.DisplayMetadata;
+import org.dspace.content.Item;
+import org.dspace.core.Context;
+
+public class DisplayItemMetadataUtils {
+
+	public static List<DisplayMetadata> getDisplayMetadata(Context context, HttpServletRequest req, Item item, String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
